fb48ce9d6cef40fa2361182f03cea1d34d42216b
Shercosta <70560582+Shercosta@users.noreply.github.com>
2023-04-27 15:33:11 +0700

    navbar ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +27,25 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col col-9">
+            <h1>Koordinat Jalan Lampung Selatan</h1>
+          </div>
+          <div className="col col-3">
+            <form className="d-flex">
+              <input
+                type="search"
+                className="form-control my-2"
+                placeholder="Search"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
